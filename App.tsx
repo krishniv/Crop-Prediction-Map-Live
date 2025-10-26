c14747a79440999151b8843373959980b19531fb
= <siddharthnahar819@gmail.com>
2025-10-26 13:53:37 -0400

    Added news button redirection

--- App.tsx
+++ App.tsx
@@ -225,13 +225,8 @@ function AppComponent() {
             <div className="header-right">
               <button
                 className="news-button"
-                onClick={() =>
-                  window.open(
-                    'https://news.google.com/search?q=agriculture+farming+news&hl=en-US',
-                    '_blank'
-                  )
-                }
-              >
+                onClick={() => window.open('/news.html', '_blank')}
+                >
                 🗞️ Farm-o-Buzz
               </button>
 
